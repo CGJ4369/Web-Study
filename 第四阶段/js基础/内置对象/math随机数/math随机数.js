c731abec6a208a94757cd9d8a46c21b06f2cabaa
CGJ4369 <1825274812@qq.com>
2022-09-24 23:38:28 +0800

    第五十八次修改

--- 第四阶段/js基础/内置对象/math随机数/math随机数.js
+++ 第四阶段/js基础/内置对象/math随机数/math随机数.js
@@ -14,3 +14,31 @@ console.log(getRandom(1, 10));
 var arr = ["张三", "114514", "仙贝"];
 // console.log(arr[getRandom(0, 2)]);
 console.log(arr[getRandom(0, arr.length - 1)]);
+
+
+//猜数字游戏
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+var random = getRandom(1, 10);
+var count = 0;
+while (true) {
+    var num = prompt("输入1~10之间的一个数字,只有5次机会");
+    if (count < 5) {
+        if (num > random) {
+            alert("你猜大了");
+            count++;
+        } else if (num < random) {
+            alert("你猜小了")
+            count++;
+        } else {
+            alert("你猜对了");
+            break;
+        }
+    }
+    else {
+        alert("超过5次了");
+        break;
+    }
+
+}
